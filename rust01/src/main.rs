43dcac7e2946fcc48293dbdd2defebda3171d273
Bruno Rocha <rochacbruno@gmail.com>
2020-01-31 20:03:31 -0300

    versao 1 usando &str

--- rust01/src/main.rs
+++ rust01/src/main.rs
@@ -1,44 +1,32 @@
 #![allow(dead_code)]
 
-#[derive(Debug, PartialEq, Eq)]
-enum Player {
-    Pedra,
-    Papel,
-    Tesoura
-}
+fn jokenpo(p1: &str, p2: &str) -> &'static str {
 
-#[derive(Debug, PartialEq, Eq)]
-enum Resultado {
-    Ganhou(Player),
-    Empatou
-}
-
-fn jokenpo(p1: Player, p2: Player) -> Resultado {
     match (p1, p2) {
-        (Player::Pedra, Player::Papel) | (Player::Papel, Player::Pedra) => Resultado::Ganhou(Player::Papel),
-        (Player::Pedra, Player::Tesoura) | (Player::Tesoura, Player::Pedra) => Resultado::Ganhou(Player::Pedra),
-        (Player::Tesoura, Player::Papel) | (Player::Papel, Player::Tesoura) => Resultado::Ganhou(Player::Tesoura),
-        (_, _) => Resultado::Empatou
+        ("pedra", "papel") | ("papel", "pedra") => "papel",
+        ("pedra", "tesoura") | ("tesoura", "pedra") => "pedra",
+        ("tesoura", "papel") | ("papel", "tesoura") => "tesoura",
+        (_, _) => "empatou"
     }
 }
 
 #[test]
 fn test_pedra_papel() {
-    assert_eq!(jokenpo(Player::Pedra, Player::Pedra), Resultado::Empatou);
+    assert_eq!(jokenpo("pedra",  "pedra"), "empatou");
 
 
-    assert_eq!(jokenpo(Player::Pedra,  Player::Pedra), Resultado::Empatou);
-    assert_eq!(jokenpo(Player::Tesoura,  Player::Tesoura), Resultado::Empatou);
-    assert_eq!(jokenpo(Player::Papel,  Player::Papel), Resultado::Empatou);
+    assert_eq!(jokenpo("pedra",  "pedra"), "empatou");
+    assert_eq!(jokenpo("tesoura",  "tesoura"), "empatou");
+    assert_eq!(jokenpo("papel",  "papel"), "empatou");
 
-    assert_eq!(jokenpo(Player::Pedra,  Player::Papel), Resultado::Ganhou(Player::Papel));
-    assert_eq!(jokenpo(Player::Papel,  Player::Pedra), Resultado::Ganhou(Player::Papel));
+    assert_eq!(jokenpo("pedra",  "papel"), "papel");
+    assert_eq!(jokenpo("papel",  "pedra"), "papel");
 
-    assert_eq!(jokenpo(Player::Pedra,  Player::Tesoura), Resultado::Ganhou(Player::Pedra));
-    assert_eq!(jokenpo(Player::Tesoura,  Player::Pedra), Resultado::Ganhou(Player::Pedra));
+    assert_eq!(jokenpo("pedra",  "tesoura"), "pedra");
+    assert_eq!(jokenpo("tesoura",  "pedra"), "pedra");
 
-    assert_eq!(jokenpo(Player::Tesoura, Player::Papel), Resultado::Ganhou(Player::Tesoura));
-    assert_eq!(jokenpo(Player::Papel, Player::Tesoura), Resultado::Ganhou(Player::Tesoura));
+    assert_eq!(jokenpo("tesoura", "papel"), "tesoura");
+    assert_eq!(jokenpo("papel", "tesoura"), "tesoura");
 }
 
 
